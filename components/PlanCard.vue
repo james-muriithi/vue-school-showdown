7bf06843854e004e3db679690f91d3c889439938
James Muriithi <26818458+james-muriithi@users.noreply.github.com>
2024-04-01 14:11:36 +0300

    workshop sections

---
+++ components/PlanCard.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="plan-card px-5 py-10 rounded-2xl bg-secondary shadow">
+    <h4 class="font-bold text text-white">
+      <img :src="plan.icon" class="inline h-5" />
+      <span class="pl-3"> {{ plan.title }} </span>
+    </h4>
+
+    <div class="mt-10">
+      <p class="text-primary text-sm mb-5">Includes 1 year access to:</p>
+
+      <div>
+        <ul class="text-gray-200 text-sm">
+          <li
+            v-for="(benefit, index) in plan.benefits"
+            :key="`benefit-${index}-${plan.id}`"
+            class="flex items-center mb-3"
+            :class="{ 'opacity-30': benefit.disabled }"
+          >
+            <IconMaterialSymbols:CheckCircle class="text-primary inline" />
+            <span class="pl-3"> {{ benefit.title }} </span>
+            <span class="ml-auto">
+              <IconMdi:CloseCircle
+                v-if="benefit.disabled"
+                class="text-primary inline"
+              />
+              <IconMdi:InformationVariantCircle v-else />
+            </span>
+          </li>
+        </ul>
+      </div>
+
+      <div v-if="plan.showMasterClassCourse" class="my-4">
+        <div class="course-card px-4 py-4 rounded-lg">
+          <div class="flex gap-x-4">
+            <div>
+              <img src="@/assets/images/vue-masterclass.svg" alt="vue masterclass" class="rounded-lg h-10 w-10">
+            </div>
+            <div>
+              <p class="text-gray-400 text-xs">
+                Enough time to watch:
+              </p>
+              <p class="text-gray-200 text-sm mt-1">
+                The Vue 3 Masterclass
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="my-4">
+        <div class="bg-dark rounded-lg px-3 py-6">
+          <p class="flex items-center">
+            <span class="font-semibold text-3xl text-white">
+              {{ plan.currency }}{{ plan.pricePerSeat }}
+            </span>
+            <span class="pl-3 text-sm text-gray-400">
+              / seat / {{ plan.priceInterval }}
+            </span>
+          </p>
+          <p class="mt-10 mb-3 font-semibold text-sm text-white">
+            How many licenses do you need?
+          </p>
+          <input
+            type="range"
+            min="5"
+            max="20"
+            value="10"
+            step="5"
+            class="w-full h-1 mb-6 rounded-lg appearance-none cursor-pointer range-sm bg-primary slider"
+          />
+          <div class="mt-3 flex justify-between items-center">
+            <div class="flex items-center">
+              <img src="@/assets/images/people.svg" alt="" class="h-4 inline" />
+              <p class="inline">
+                <span class="font-bold pl-2 text-white text-xl"> 12 </span>
+                <span class="text-gray-400 text-sm pl-2"> seats </span>
+              </p>
+            </div>
+            <p class="text-primary text-sm font-semibold">26% discount</p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <button
+          class="bg-primary w-full py-3 rounded-lg font-semibold text-gray-700"
+        >
+          Start learning
+        </button>
+      </div>
+      <div class="mt-3 text-center">
+        <router-link to="#" class="text-sm font-semibold text-primary">
+          Refer your manager
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+import type Plan from "../types/Plan";
+
+interface Props {
+  plan: Plan;
+}
+defineProps<Props>();
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables";
+
+.plan-card {
+  .slider {
+    &::-webkit-slider-thumb {
+      @apply rounded-full h-3 w-3 appearance-none bg-primary;
+    }
+  }
+  .course-card {
+    background: rgba($color: $primary, $alpha: 0.2);
+  }
+}
+</style>
---
+++ components/landing/Workshops.vue
@@ -0,0 +1,68 @@
+<template>
+  <section class="my-10 max-w-screen-xl mx-auto px-5 xl:px-3">
+    <div class="flex flex-wrap">
+      <h2 class="title text-white">Available Workshops</h2>
+      <div class="ml-auto hidden md:block">
+        <button class="rounded-lg px-5 py-3 bg-primary text-sm">
+          See all workshops
+        </button>
+      </div>
+    </div>
+    <div class="mt-10">
+      <div class="grid grid-cols-3">
+        <div class="workshop-card rounded-xl">
+          <div class="card-header py-4 px-5 rounded-t-xl">
+            <p class="text-gray-200">
+              <IconMaterialSymbols:CalendarToday class="inline" />
+              <span class="pl-2 text-sm">
+                December 14th - 11:00 AM (GMT-3)
+              </span>
+            </p>
+          </div>
+          <div class="card-body px-5 py-6 bg-secondary rounded-b-xl">
+            <div class="flex gap-x-5">
+              <p class="text-gray-400 text-sm">
+                <IconMaterialSymbols:PlayCircle class="inline" />
+                LIVE Training
+              </p>
+              <p class="text-gray-400 text-sm">
+                <IconMaterialSymbols:NestClockFarsightAnalogRounded
+                  class="inline"
+                />
+                15 Hours
+              </p>
+            </div>
+            <h5 class="text-gray-200 mt-3 text-xl">Vue.js Fundamentals</h5>
+
+            <p class="text-gray-400 text-sm mt-2 line-clamp-2">
+              A progressive framework for building user interfaces. Unlike other
+              monolithic frameworks, Vue is designed from the ground up to be
+              incrementally
+            </p>
+            <div class="mt-3">
+                <div class="flex items-center text-gray-400 text-sm">
+                    <img src="@/assets/images/steve-avatar.svg" alt="Steve Popoola" class="h-6 w-6 rounded-full mr-2">
+                    Steve Popoola
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.workshop-card {
+  .card-header {
+    background: linear-gradient(
+      90deg,
+      rgba(79, 8, 230, 0.5) 0%,
+      rgba(255, 73, 117, 0.5) 96.56%
+    );
+  }
+}
+</style>
