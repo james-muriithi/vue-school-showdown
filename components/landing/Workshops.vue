c6fbf5338a5f40c9ad84d20a7ca60a1ce1c093a7
James Muriithi <26818458+james-muriithi@users.noreply.github.com>
2024-04-01 14:54:54 +0300

    workshops carousel

--- components/landing/Workshops.vue
+++ components/landing/Workshops.vue
@@ -9,60 +9,73 @@
       </div>
     </div>
     <div class="mt-10">
-      <div class="grid grid-cols-3">
-        <div class="workshop-card rounded-xl">
-          <div class="card-header py-4 px-5 rounded-t-xl">
-            <p class="text-gray-200">
-              <IconMaterialSymbols:CalendarToday class="inline" />
-              <span class="pl-2 text-sm">
-                December 14th - 11:00 AM (GMT-3)
-              </span>
-            </p>
-          </div>
-          <div class="card-body px-5 py-6 bg-secondary rounded-b-xl">
-            <div class="flex gap-x-5">
-              <p class="text-gray-400 text-sm">
-                <IconMaterialSymbols:PlayCircle class="inline" />
-                LIVE Training
-              </p>
-              <p class="text-gray-400 text-sm">
-                <IconMaterialSymbols:NestClockFarsightAnalogRounded
-                  class="inline"
-                />
-                15 Hours
-              </p>
-            </div>
-            <h5 class="text-gray-200 mt-3 text-xl">Vue.js Fundamentals</h5>
-
-            <p class="text-gray-400 text-sm mt-2 line-clamp-2">
-              A progressive framework for building user interfaces. Unlike other
-              monolithic frameworks, Vue is designed from the ground up to be
-              incrementally
-            </p>
-            <div class="mt-3">
-                <div class="flex items-center text-gray-400 text-sm">
-                    <img src="@/assets/images/steve-avatar.svg" alt="Steve Popoola" class="h-6 w-6 rounded-full mr-2">
-                    Steve Popoola
-                </div>
-            </div>
-          </div>
-        </div>
+      <div class="">
+        <Carousel :breakpoints="breakpoints" class="cursor-pointer h-72">
+          <slide
+            v-for="(workshop, index) in workshops"
+            :key="`${workshop.title}-${index}`"
+            class="h-full"
+          >
+            <WorkshopCard :workshop="workshop" />
+          </slide>
+        </Carousel>
       </div>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
+import { Carousel, Slide } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
+import type Workshop from "@/types/Workshop";
+
+const breakpoints = {
+  0: {
+    itemsToShow: 1,
+    snapAlign: "start",
+  },
+
+  540: {
+    itemsToShow: 1.5,
+    snapAlign: "start",
+  },
+
+  840: {
+    itemsToShow: 2.5,
+    snapAlign: "start",
+  },
+};
+
+const workshops: Workshop[] = [
+  {
+    title: "Vue.js Fundamentals",
+    description:
+      "A progressive framework for building user interfaces. Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally",
+    type: "LIVE Training",
+    date: "December 14th - 11:00 AM (GMT-3)",
+    duration: "15 Hours",
+  },
+  {
+    title: "Vue 3 and Composition API",
+    description:
+      "Take a deep dive into the new and exciting features of Vue 3. Learn about Portals, Suspense, Composition API, Fragments, and much much more",
+    type: "LIVE Training",
+    date: "December 14th - 11:00 AM (GMT-3)",
+    duration: "15 Hours",
+  },
+  {
+    title: "Vue 3 and Composition API",
+    description:
+      "Take a deep dive into the new and exciting features of Vue 3. Learn about Portals, Suspense, Composition API, Fragments, and much much more",
+    type: "LIVE Training",
+    date: "December 14th - 11:00 AM (GMT-3)",
+    duration: "15 Hours",
+  },
+];
 </script>
 
 <style lang="scss" scoped>
-.workshop-card {
-  .card-header {
-    background: linear-gradient(
-      90deg,
-      rgba(79, 8, 230, 0.5) 0%,
-      rgba(255, 73, 117, 0.5) 96.56%
-    );
-  }
+:deep(ol.carousel__track) {
+  @apply flex gap-x-5;
 }
 </style>
---
+++ types/Workshop.ts
@@ -0,0 +1,7 @@
+export default interface Workshop {
+  duration: string;
+  title: string;
+  date: string;
+  description: string;
+  type: string;
+}
