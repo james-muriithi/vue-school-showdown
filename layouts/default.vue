616419e2fa42bfccfb8c0fb3f9d314a55a8e907d
James Muriithi <26818458+james-muriithi@users.noreply.github.com>
2024-04-01 13:54:20 +0300

    add a default layout

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-darker">
+    <TheNavbar />
+    <div class="">
+      <slot />
+    </div>
+    <TheFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
